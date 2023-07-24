c1591563b6b190bab5cdfc4b454aa321bf5c2254
ManojTGN <manoj.thunderviz@gmail.com>
2023-07-24 11:22:45 +0530

    complete book ride
    update frontend bookride
    @todo backend

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,22 @@
 require('dotenv').config()
 
 const cookieParser = require("cookie-parser");
+const jsonwebtoken = require('jsonwebtoken');
 const bodyParser = require("body-parser");
 const color = require('./src/colors');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
-const jsonwebtoken = require('jsonwebtoken');
 
 const app = express();
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.post('/register',(req,res)=>{
-    
     let usr = jsonwebtoken.decode(req.body.credential);
     if(usr){
         res.cookie('login',req.body.credential);
@@ -23,6 +24,14 @@ app.post('/register',(req,res)=>{
     }else{
         res.sendStatus(401);
     }
-})
+});
+
+app.post('/',(req,res)=>{
+    if('bookRide' in req.body){
+        console.log(req.body)
+        res.sendStatus(200);
+        return true;
+    }
+});
 
 app.listen(process.env.PORT || 3000, ()=>{console.log(`\n${color.FgGreen}AceRide${color.Reset} Backend Server Started At Port ${color.FgYellow}${process.env.PORT || 3000}\n${color.Reset} - ${color.FgBlue}http://localhost:${process.env.PORT || 3000}/\n${color.Reset}`)});
--- frontend/src/components/MainContent/Book/SideBar/Payement.js
+++ frontend/src/components/MainContent/Book/SideBar/Payement.js
@@ -1,40 +1,84 @@
 import axios from 'axios';
+import { useEffect, useState, useRef } from 'react';
 import env from "react-dotenv";
 import {useNavigate} from 'react-router-dom'
 
 export default function Payment(props){
     let navigate = useNavigate();
-    if(props.paymentType === 'cor'){
+    const [loading,setLoading] = useState(false);
+    const [booked,setBooked] = useState(null);
+    let iconTick = useRef();
+
+    function bookRide(){
         axios({
             method: 'POST',
-            url:`http://localhost:${env.SERVER_PORT}/register`,
+            url:`http://localhost:${env.SERVER_PORT}/`,
             headers:{"Content-Type": "application/json" },
             data:{bookRide:true,...props.data}
         }).then((res)=>{
-            console.log(res);
-        });
+            if(res.status === 200){
+                if(!booked) setBooked(true);
+                if(loading) setLoading(false);
+                setTimeout(()=>{ iconTick.current.className += ' scale-150';},300);
+            }else{
+                if(!booked) setBooked(false);
+                if(loading) setLoading(false);
+            }
+        });   
+        if(!loading) setLoading(true);
     }
+
+    useEffect(()=>{
+        if(props.data.paymentType === 'cor') bookRide();
+    },[false]);
+
     return (
-        <div className="font-bold text-black text-left px-10 flex flex-col">
-            <div className="h-80 flex flex-col gap-2 overflow-scroll no-scrollbar::-webkit-scrollbar no-scrollbar">
-                <p className="text-center">{props.data.paymentType==='cor'?<i className="fa-solid fa-heart"></i>:<i className="fa-brands fa-google-pay fa-2xl"></i>} {props.data.paymentType==='cor'?'Placing Your Ride':'Payment'}</p>
-                <img src="./images/exampleQrCode.webp" alt="GooglePay QrCode"/>
-            </div>
-            <div className="h-52 flex flex-col gap-2">
-                Pay By Scaning With Your Gpay QR Scanner
-                <button className="w-full text-start h-10 p-2 bg-gradient-to-r from-emerald-200 to-transparent rounded cursor-pointer hover:from-emerald-400 transition-all duration-100 ease-in-out hover:scale-105">
-                    <i className="fa-solid fa-wallet"></i> Pay With GPay Wallet
-                </button>
-                <button onClick={()=>navigate('/newRide')} className="w-full text-start h-10 p-2 bg-gradient-to-r from-emerald-200 to-transparent rounded cursor-pointer hover:from-emerald-400 transition-all duration-100 ease-in-out hover:scale-105">
-                <i className="fa-solid fa-circle-xmark"></i> Cancel Ride
-                </button>
-            </div>
-            <div className="h-56 w-full flex items-end justify-center">
-                <div className="flex w-full p-4 bg-gradient-to-r from-emerald-100 to-emerald-200 rounded">
-                    <div className="w-1/6"> <i className="font-bold fa-solid fa-circle-notch fa-spin"></i> </div>
-                    <p className="w-full text-right">Booking Your Ride</p>
+        <>
+        {booked===true?
+        <>
+        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl text-emerald-100'>
+        <i className="fa-solid fa-certificate fa-2xl fa-spin"></i>
+        </div>
+        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl text-emerald-500'>
+            <i ref={iconTick} className={"fa-solid fa-check fa-2xl transition-all duration-500 "}></i>
+        </div>
+        <div className='absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl text-emerald-500 font-bold'>
+            Ride Booked Successfull
+        </div>
+        <div>
+            <div className='w-full h-96 bg-gradient-to-b from-transparent to-white'></div>
+            <div className='w-full h-96 bg-gradient-to-b to-emerald-400 from-white flex items-end'>
+                <div onClick={()=>navigate('/yourRides')} className="flex w-full p-4 bg-gradient-to-r from-emerald-100 to-emerald-200 rounded cursor-pointer">
+                    <div className="w-1/6"> <i className="fa-regular fa-square-check"></i> </div>
+                    <p className="w-full text-right">Ride Booked</p>
                 </div>
             </div>
         </div>
+        </>
+        :booked===false?null:null}
+        {booked===null?<div className="font-bold text-black text-left px-10 flex flex-col">
+            <div className="h-80 flex flex-col gap-2 overflow-scroll no-scrollbar::-webkit-scrollbar no-scrollbar">
+                <p className="text-center">{props.data.paymentType==='cor'?null:<i className="fa-brands fa-google-pay fa-2xl"></i>} {props.data.paymentType==='cor'?null:'Payment'}</p>
+                {props.data.paymentType === 'cor'?null:<img src="./images/exampleQrCode.webp" alt="GooglePay QrCode"/>}
+            </div>
+            <div className="h-52 flex flex-col gap-2">
+                {props.data.paymentType === 'cor'?null:<>
+                <p>Pay By Scaning With Your Gpay QR Scanner</p>
+                <button onClick={()=>bookRide()} className="w-full text-start h-10 p-2 bg-gradient-to-r from-emerald-200 to-transparent rounded cursor-pointer hover:from-emerald-400 transition-all duration-100 ease-in-out hover:scale-105">
+                    <i className="fa-solid fa-wallet"></i> Pay With GPay Wallet
+                </button>
+                <button onClick={()=>navigate('/dashboard')} className="w-full text-start h-10 p-2 bg-gradient-to-r from-emerald-200 to-transparent rounded cursor-pointer hover:from-emerald-400 transition-all duration-100 ease-in-out hover:scale-105">
+                <i className="fa-solid fa-circle-xmark"></i> Cancel Ride
+                </button></>}
+            </div>
+            <div className="h-56 w-full flex items-end justify-center">
+                {loading?
+                <div className="flex w-full p-4 bg-gradient-to-r from-emerald-100 to-emerald-200 rounded">
+                    <div className="w-1/6"> <i className="font-bold fa-solid fa-circle-notch fa-spin"></i> </div>
+                    <p className="w-full text-right">Booking Your Ride</p>
+                </div>:null}
+            </div>
+        </div>:null}
+        </>
     );
 }
